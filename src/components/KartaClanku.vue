2724094cd7c79113a7b0742fc0a543774abc5b65
Michal Raus <raus.michal@email.cz>
2025-03-01 01:11:54 +0100

    CSS prototip KARTY CLANKU

---
+++ src/components/KartaClanku.vue
@@ -0,0 +1,117 @@
+
+
+
+
+
+<template>
+<div class="karta">
+<div class="con">
+
+<div class="img-box">
+<p>Obrázek z API - 300px X auto</p>
+</div>
+
+<div class="con-other">
+<p>TRAVEL</p>
+<p>Nadpis - bude dodán z JSON API</p>
+<p class="text">Popisek bude dodán z JSON API - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</p>
+
+<div class="con-button">
+<button type="button" title="Číst víc" ><span>Read More</span></button>
+</div>
+</div>
+
+
+</div>
+
+</div>
+
+
+</template>
+
+<script setup lang="ts">
+const name = 'KartaClanku';
+</script>
+
+<style scoped>
+
+.karta
+{
+display:flex;
+width:300px;
+height:auto;
+background-color:transparent;
+margin-bottom:1rem;
+}
+
+.img-box /* tato class nejspíš zanikne jakmile se tam dodá obrázek */
+{
+width:100%;
+min-width:100%;
+height:300px; /* pouze dočasně - jakmile to bude img - bude výška auto */
+background-color:aqua;
+margin-bottom:1rem;
+}
+
+
+.con
+{
+display:flex;
+flex-wrap:wrap;
+margin-bottom:3rem;
+}
+
+.con-other
+{
+align-self:flex-end;
+}
+
+.con div p
+{
+color:black;
+font-size:1.5rem;
+font-weight:500;
+text-align:center;
+margin-bottom:2rem;
+}
+
+.con div p:first-child
+{
+font-size:.8rem;
+text-transform:uppercase;
+margin-bottom:1rem;
+font-weight:200;
+}
+
+.con div p.text
+{
+font-size:.9rem;
+margin-bottom:1rem;
+font-weight:200;
+text-align:justify;
+}
+
+.con-button
+{
+display:flex;
+justify-content:center;
+}
+
+.con div button
+{
+font-size:.9rem;
+color:black;
+background:none;
+border:none;
+width:auto;
+padding:.7rem 1rem;
+filter:transformY(-0.25rem);
+}
+
+.con button span
+{
+display:block;
+transform: translateY(0.1rem);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,24 @@
 </div>
 
 
+<div class="con-clanky">
+<KartaClanku />
+<KartaClanku />
+<KartaClanku />
+<KartaClanku />
+<KartaClanku />
+<KartaClanku />
+<KartaClanku />
+<KartaClanku />
+<KartaClanku />
+</div>
+
 </template>
 
 <script setup lang="ts">
 import KartaProduktu from '../components/KartaProduktu.vue'
+import KartaClanku from '../components/KartaClanku.vue'
+
 
 const name = 'Home';
 
@@ -24,8 +38,49 @@ display:flex;
 flex-wrap:wrap;
 justify-content:center;
 align-items:center;
-gap:1rem;
+gap:.5rem;
 margin-bottom:1rem;
 }
+
+
+.con-clanky
+{
+display:flex;
+flex-wrap:wrap;
+justify-content:center;
+height:auto;
+margin:5rem 0;
+}
+
+/* pokud je obrazovka větší než */
+@media only screen and (min-width:600px)
+{
+.con-clanky
+{
+columns:2;
+height:auto;
+gap:1rem;
+}
+}
+
+/* pokud je obrazovka větší než */
+@media only screen and (min-width:900px)
+{
+.con-clanky
+{
+columns:3;
+}
+}
+
+/* pokud je obrazovka větší než */
+@media only screen and (min-width:1000px)
+{
+.con-clanky
+{
+margin:5rem 15vw;
+}
+}
+
+
 
 </style>
