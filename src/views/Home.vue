88c4735a8276493fc09037ad51a44a5eaf17bdda
Michal Raus <raus.michal@email.cz>
2025-03-01 08:02:23 +0100

    Pripojeni JSON souboru pro karty Produktu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,6 +26,60 @@
 import KartaProduktu from '../components/KartaProduktu.vue'
 import KartaClanku from '../components/KartaClanku.vue'
 
+class Produkty{
+
+loadData()
+{
+// načtení z JSON časy, které jsou již rezervované
+
+// Asynchronní funkce pro načtení JSON souboru pomocí fetch
+const fetchJSON=async():Promise<any> => {
+const jsonFilePath="./data/produkty.json"; // cesta k JSON souboru
+try{
+const response=await fetch(jsonFilePath); // načítání dat ze souboru JSON
+if(!response.ok){
+throw new Error("Síťová odpověď nebyla v pořádku"); // chyba při načítání
+}
+let jsonData=await response.json(); // převzetí dat do proměnné
+
+// Pokud JSON neobsahuje pole 'data' nebo pokud je prázdné, nastavíme prázdné pole
+if(!jsonData||!jsonData.data||!Array.isArray(jsonData.data))
+{
+jsonData={data:[]};  // Zajistíme, že data budou vždy pole
+}
+return jsonData; // vrací načtená data
+}catch(error) {
+console.error("Chyba při načítání JSON:", error);
+return {data:[]}; // Vrátí prázdný objekt, pokud dojde k chybě
+}
+};
+
+setTimeout(async()=>{
+// Načtení JSON dat ze serveru
+
+const jsonData=await fetchJSON(); // Načtení JSON dat z servru
+
+// Pokud byla data úspěšně načtena, pokračuj ve zpracování
+if(jsonData){
+jsonData.data.forEach((item:{id:number,src:string,title:string,nadpis:string})=>{
+const dateArray:[number,string,string,string]=[item.id,item.src,item.title,item.nadpis];
+
+console.log(dateArray);
+
+});
+}
+
+// zde budou případná operace po zpracování JSON
+
+},0);  // Použití setTimeout k oddělení asynchronní operace
+
+};
+
+};
+
+const produkty=new Produkty;
+
+produkty.loadData();
 
 const name = 'Home';
 
