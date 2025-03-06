acda7b7cebb688491db5161fef2d7bd115540756
Michal Raus <raus.michal@email.cz>
2025-03-06 22:15:39 +0100

    Odstraneni chyb pri build

--- src/views/Home.vue.js
+++ src/views/Home.vue.js
@@ -1,4 +1,5 @@
-import KartaProduktu from '../components/KartaProduktu.vue';
+import KartaProduktu from "../components/KartaProduktu.vue";
+import Monsory from "../components/Monsory.vue";
 import { useDataLoader } from "@/services/dataService";
 const { produkty, karty } = useDataLoader();
 ; /* PartiallyEnd: #3632/scriptSetup.vue */
@@ -30,21 +31,21 @@ for (const [item, index] of __VLS_getVForSourceType((__VLS_ctx.produkty))) {
         title: (item.title),
     }, ...__VLS_functionalComponentArgsRest(__VLS_0));
 }
-const __VLS_3 = {}.Monsory;
-/** @type {[typeof __VLS_components.Monsory, ]} */ ;
+/** @type {[typeof Monsory, ]} */ ;
 // @ts-ignore
-const __VLS_4 = __VLS_asFunctionalComponent(__VLS_3, new __VLS_3({
+const __VLS_3 = __VLS_asFunctionalComponent(Monsory, new Monsory({
     karty: (__VLS_ctx.karty),
 }));
-const __VLS_5 = __VLS_4({
+const __VLS_4 = __VLS_3({
     karty: (__VLS_ctx.karty),
-}, ...__VLS_functionalComponentArgsRest(__VLS_4));
+}, ...__VLS_functionalComponentArgsRest(__VLS_3));
 /** @type {__VLS_StyleScopedClasses['flex']} */ ;
 var __VLS_dollars;
 const __VLS_self = (await import('vue')).defineComponent({
     setup() {
         return {
             KartaProduktu: KartaProduktu,
+            Monsory: Monsory,
             produkty: produkty,
             karty: karty,
         };
